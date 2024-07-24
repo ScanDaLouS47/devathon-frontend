9a7d3b962cbb47831a525f2944120c4531dc4951
Joselu <joseluissgz97@gmail.com>
2024-07-24 18:45:09 +0200

    feat: add form input componetn and styles

---
+++ src/components/formInput/FormInput.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FieldError } from 'react-hook-form';
+import { WarningIcon } from '../icons/WarningIcon';
+import './formInput.scss';
+
+type InputProps = React.ComponentProps<'input'> & {
+  label: string;
+  error: FieldError | undefined;
+};
+export const FormInput = React.forwardRef(
+  ({ label, error, ...inputProps }: InputProps, ref: React.Ref<HTMLInputElement>) => {
+    const hasError = error ? 'row__error' : '';
+
+    return (
+      <div className="row">
+        <label className={`row__label ${hasError}`} htmlFor={inputProps.id}>
+          {label}*
+        </label>
+        <div className={`row__container ${hasError}`}>
+          <input ref={ref} className="row__input" autoComplete="off" {...inputProps} />
+          {error && <WarningIcon className="row__icon" />}
+        </div>
+        <span className="row__message">{error?.message} </span>
+      </div>
+    );
+  },
+);
