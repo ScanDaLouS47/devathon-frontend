dae73a1363ed68547e0cbf45586acc0b922022b0
Joselu <joseluissgz97@gmail.com>
2024-08-05 22:15:46 +0200

    refactor: Add lazy loading for images in PrivateRoutes

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -1,14 +1,25 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 import { useAuth } from '../auth/hook/useAuth';
+import { Aside } from '../panel/components/Aside';
 
 export const PrivateRoute = () => {
   const { authState } = useAuth();
+  const navigate = useNavigate();
+  console.log(authState);
 
-  if (authState.user.role === 'admin') {
-    return <Outlet />;
-  } else if (authState.user.role === 'user') {
-    return <Outlet />;
-  }// else  {
-  //   return <Navigate to="/error/unauthorized" replace />;
-  // }
+  useEffect(() => {
+    if (!authState.logged) {
+      navigate('/auth/login');
+      return;
+    }
+    if (authState.user?.role !== 'admin') navigate('/panel/user');
+  }, [authState.user?.role]);
+
+  return (
+    <section>
+      <Aside />
+      <Outlet />
+    </section>
+  );
 };
