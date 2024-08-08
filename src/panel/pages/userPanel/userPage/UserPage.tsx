2dfba1ec60991b7b511be9778ad01af775712b94
Marcos Lambir Torres <markos_laviana@yahoo.es>
2024-08-09 00:07:23 +0200

    refactor: delete unused routes

--- src/panel/pages/userPanel/userPage/UserPage.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './userPage.module.scss';
-
-export const UserPage = () => {
-  return (
-    <div className={`${styles.user} wrapper`}>
-      <div className="register__container">UserPage</div>
-    </div>
-  );
-};
