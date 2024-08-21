028fa040e5cce393f92341deb623d466431e5a1b
Marcos Lambir Torres <markos_laviana@yahoo.es>
2024-08-21 21:27:00 +0200

    refactor: update MyReservations

--- src/panel/pages/userPanel/myReservations/MyReservations.tsx
+++ src/panel/pages/userPanel/myReservations/MyReservations.tsx
@@ -3,11 +3,13 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { toast } from 'react-toastify';
 import { FormInput } from '../../../../components/formInput/FormInput';
-import { IRespBooking } from '../../../../interfaces/';
+import { BookingData, IRespBooking } from '../../../../interfaces/';
 import { ApiError } from '../../../../utils/apiError';
 import { fetchApi } from '../../../../utils/fetchApi';
 import { myReservationsType, myReservationsSchema } from './myReservationsSchema';
 import { useAuth } from '../../../../auth/hook/useAuth';
+import { useEffect, useState } from 'react';
+import { Pagination } from '../../../../components/pagination/Pagination';
 
 export const MyReservations = () => {
   // GET a /api/v1/mybookings/62?filter=2024-08-26&active=active&number=5
@@ -20,17 +22,24 @@ export const MyReservations = () => {
     resolver: zodResolver(myReservationsSchema),
     mode: 'onChange',
   });
+
   const { authState } = useAuth();
+  const [dataBookings, setDataBookings] = useState<BookingData[]>();
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
+
+  useEffect(() => {
+    (async () => {
+      await fetchApi<IRespBooking>(`/api/v1/mybookings`, 'GET', `${authState.user?.id}`, null, true, true)
+        .then((r) => setDataBookings(r.data))
+        .catch((e) => console.error(e));
+    })();
+  }, [authState.user?.id]);
 
   const handleMyBookings: SubmitHandler<myReservationsType> = async (data) => {
     const queryParamsFilter = data.date ? `filter=${data.date}` : '';
     const queryParamsPersons = data.numberOfPersons ? `persons=${data.numberOfPersons}` : '';
-
     const toastInfo = toast.loading('Loading...');
-
-    console.log(authState.user?.id);
-    console.log(data);
-
     try {
       const myBookingsResp = await fetchApi<IRespBooking>(
         `/api/v1/mybookings`,
@@ -54,7 +63,8 @@ export const MyReservations = () => {
         });
       }
 
-      console.log(myBookingsResp);
+      console.log('ON MY BACKEND', myBookingsResp);
+      setDataBookings(myBookingsResp.data);
     } catch (error) {
       if (error instanceof ApiError) {
         toast.error(error.message, { autoClose: 3000 });
@@ -62,6 +72,13 @@ export const MyReservations = () => {
       }
     }
   };
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  const actualIndex = (currentPage - 1) * itemsPerPage;
+  const selectedItemsToView = dataBookings?.slice(actualIndex, actualIndex + itemsPerPage);
 
   return (
     <div className={styles.myReservations}>
@@ -83,20 +100,40 @@ export const MyReservations = () => {
             type="text"
             {...register('numberOfPersons')}
           />
-          {/* <FormInput
-            label="Reservation Number"
-            error={errors['reservationNumber']}
-            id="reservation-number"
-            type="text"
-            placeholder="#0000000000"
-            {...register('reservationNumber')}
-          /> */}
           <div className={styles.form__btnContainer}>
             <button className={styles.form__btn} type="submit">
               Search
             </button>
           </div>
         </form>
+        <table className={styles.table}>
+          <thead>
+            <tr className={styles.table__tr}>
+              <th>ID</th>
+              <th>TURNOS</th>
+              <th>FECHA</th>
+              {/* <th>STATUS</th> */}
+            </tr>
+          </thead>
+          <tbody>
+            {selectedItemsToView &&
+              selectedItemsToView.map((e, i) => {
+                return (
+                  <tr key={i} className={styles.table__tr}>
+                    <td>{e.id}</td>
+                    <td>{e.shift}</td>
+                    <td>{String(e.reservationDate)}</td>
+                    {/* <td>{e.status}</td> */}
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+        <Pagination
+          totalItems={dataBookings ? dataBookings.length : 0}
+          itemsPerPage={itemsPerPage}
+          onPageChange={handlePageChange}
+        />
       </div>
     </div>
   );
