e85cbf736d177a597731ca1af0037e384bd81c22
Joselu <joseluissgz97@gmail.com>
2024-08-22 19:30:49 +0200

    feat: Add landing page with reservation app description and images

---
+++ src/landing/LandingPage.tsx
@@ -0,0 +1,38 @@
+import { NavLink } from 'react-router-dom';
+import style from './landingPage.module.scss';
+
+export const LandingPage = () => {
+  return (
+    <section className={`${style.landing} wrapper`}>
+      <div className={style.landing__left}>
+        <div className={style.landing__content}>
+          <h2 className={style.landing__title}>
+            Effortless <span className={style.landing__span}>Reservations</span>, Anytime, Anywhere
+          </h2>
+
+          <p className={style.landing__description}>
+            Discover the ultimate reservarion app, where booking your next experience is as easy as a tap.
+            Seamless, reliable, and alwayas at your fingertips.
+          </p>
+
+          <NavLink to={'/auth/login'} className={style.landing__btn}>
+            Start Reservation
+          </NavLink>
+        </div>
+      </div>
+
+      <div className={style.landing__right}>
+        <div className={style.product}>
+          <div
+            className={style.product__image}
+            style={{ '--url': `url('./images/dish_1.webp')` } as React.CSSProperties}
+          ></div>
+          <div
+            className={style.product__image}
+            style={{ '--url': `url('./images/dish_2.webp')` } as React.CSSProperties}
+          ></div>
+        </div>
+      </div>
+    </section>
+  );
+};
