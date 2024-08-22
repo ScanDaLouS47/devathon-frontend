52115b0f70ef85678ffc2049b31e803b24c3defc
mbeas2021 <maicojobs@gmail.com>
2024-08-22 17:12:43 -0300

    dashboard admin

---
+++ src/components/dashboard/DonutChart.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import Chart from 'react-apexcharts';
+
+export const DonutChart = ({data}) => {
+    console.log(data, 'DATA DONUT')
+    let dataSeries = [];
+    let dataLabels = [];
+    data.forEach(element => {
+        dataSeries.push(element.total);
+        dataLabels.push(element.shift_name);
+    });
+
+    const [chartData] = useState({
+        options: {
+            labels: dataLabels, // ['Turno 1','Turno 2', 'Turno 3', 'Turno 4'],
+            title: {
+                text: 'Reservas por turno'
+            }
+        },
+        series: dataSeries // [44,55,15,12],
+    })
+
+
+
+  return (
+    <div style={{ width: '50%', float: 'left'}}>
+      <Chart
+        options={chartData.options}
+        series={chartData.series}
+        type="donut"
+        height={350}
+      />
+    </div>
+  )
+}
