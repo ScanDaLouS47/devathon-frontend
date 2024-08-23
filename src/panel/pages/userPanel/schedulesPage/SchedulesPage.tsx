1ba39d2ef07d2b61d2eda76cdfd1da2943b82a27
Joselu <joseluissgz97@gmail.com>
2024-08-23 16:46:53 +0200

    feat: AddhandleFilterBookings

--- src/panel/pages/userPanel/schedulesPage/SchedulesPage.tsx
+++ src/panel/pages/userPanel/schedulesPage/SchedulesPage.tsx
@@ -1,65 +1,91 @@
-import { EventClickArg } from '@fullcalendar/core/index.js';
+import { DatesSetArg, EventClickArg } from '@fullcalendar/core/index.js';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import FullCalendar from '@fullcalendar/react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { AppDispatch } from '../../../../app/store';
 import { Select } from '../../../../components/select/Select';
 import { getBookingsData, setPeople } from '../../../../features/bookins/bookinsSlice';
 import { getAllBookings } from '../../../../features/bookins/bookinsThunk';
+import { CalendarInfo } from '../../../components/calendarInfo/CalendarInfo';
 import { EventContent } from '../../../components/eventContent/EventContent';
 import { KidIcon } from '../../../components/icons/KidIcon';
+import { useSchedulesForm } from '../../../hooks/useSchedulesForm';
 import './schedulesPage.scss';
 
 export const SchedulesPage = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch<AppDispatch>();
   const bookingsEvents = useSelector(getBookingsData);
+  const { persons, setAdults, setKids } = useSchedulesForm();
 
   const handleEventClick = (info: EventClickArg) => {
     const eventStatus = info.event._def.extendedProps.status;
+    const personsSelected = persons.adults + persons.kids;
 
     if (eventStatus === 'full' || eventStatus === 'expired') return;
     navigate(`/panel/user/new-booking/${info.event._def.publicId}`);
-    dispatch(setPeople(4));
+    dispatch(setPeople(personsSelected));
   };
+  const handleFilterBookings = async (event: React.FormEvent<HTMLFormElement>) => {
+    const toastInfo = toast.loading('Filtering...');
+    event.preventDefault();
+    const totalPersons = Number(persons.adults + persons.kids);
+    const date = new Date().toISOString().slice(0, 7);
 
+    await dispatch(getAllBookings({ date, totalPersons }));
+
+    toast.update(toastInfo, {
+      render: 'Filter completed!',
+      type: 'success',
+      isLoading: false,
+      autoClose: 1000,
+    });
+  };
+  const handleDatesSet = async (arg: DatesSetArg) => {
+    // let currentDate = new Date();
+    // const currentMonth = new Date().toISOString().slice(0, 7);
+
+    const date = arg.startStr.slice(0, 7);
+    console.log(date);
+  };
   useEffect(() => {
-    dispatch(getAllBookings());
-  }, [dispatch]);
+    dispatch(getAllBookings({}));
+  }, []);
   return (
     <div className="schedulesPage">
       <header className="schedulesPage__header">
-        <form className="search">
+        <form className="search" onSubmit={handleFilterBookings}>
           <div className="search__container">
             <KidIcon className="schedulesPage__icon" />
-            <Select id="adults">
+            <Select
+              id="adults"
+              value={persons.adults}
+              onChange={(event) => setAdults(Number(event.target.value))}
+            >
               <option value="1">1</option>
-              <option value="1">2</option>
-              <option value="1">3</option>
-              <option value="1">4</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
             </Select>
           </div>
           <div className="search__container">
             <KidIcon className="schedulesPage__icon" />
 
-            <Select id="kids">
+            <Select id="kids" value={persons.kids} onChange={(event) => setKids(Number(event.target.value))}>
               <option value="1">1</option>
-              <option value="1">2</option>
-              <option value="1">3</option>
-              <option value="1">4</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
             </Select>
           </div>
           <button className="search__btn" type="submit">
             Search
           </button>
         </form>
-
-        <ul className="schedulesPage__info">
-          <li>Full</li>
-          <li>Available</li>
-        </ul>
+        <CalendarInfo />
       </header>
       <div className="calendar">
         <FullCalendar
@@ -74,6 +100,7 @@ export const SchedulesPage = () => {
           }}
           eventContent={EventContent}
           eventClick={handleEventClick}
+          datesSet={handleDatesSet}
         />
       </div>
     </div>
